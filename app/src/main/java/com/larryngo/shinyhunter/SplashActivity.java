25ed08bd5e9859ea4bb512ef42298c5cfa46e40e
larryngo97 <larryngo97@gmail.com>
2020-09-18 21:49:30 -0500

    added splash screen

---
+++ app/src/main/java/com/larryngo/shinyhunter/SplashActivity.java
@@ -0,0 +1,26 @@
+package com.larryngo.shinyhunter;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash_screen);
+
+        new Handler(Looper.getMainLooper()).postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashActivity.this, HomeActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 1500);
+    }
+}
